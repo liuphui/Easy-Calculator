36019092e0d7e62f067b54a89573edf2ff054510
Peng Hui Liu <liup1@tcd.ie>
2026-01-20 22:07:19 +0000

    Fixed draggable region and button pressing logic

--- renderer.js
+++ renderer.js
@@ -3,6 +3,18 @@ const screenEl = document.getElementById("screen");
 const historyEl = document.getElementById("history");
 const buttons = document.querySelectorAll("button");
 
+document.getElementById("minimize")?.addEventListener("click", () => {
+    window.windowControls?.minimize?.();
+})
+
+document.getElementById("maximize")?.addEventListener("click", () => {
+    window.windowControls?.maximise?.();
+})
+
+document.getElementById("close")?.addEventListener("click", () => {
+    window.windowControls?.close?.();
+})
+
 let expr = "";
 let lastResult = null;
 
